05b8c312c100a67f499b5af988bd7014c05aa7e0
Takuya Ueda <uedatakuya@gmail.com>
2015-10-19 00:39:53 +0900

    ディレクトリを対象にできるようにした

--- imgconv/ja/answer/src/cmd/imgconv/main.go
+++ imgconv/ja/answer/src/cmd/imgconv/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"image"
@@ -8,7 +9,9 @@ import (
 	"image/png"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
+	"text/template"
 
 	"../../imgconv"
 )
@@ -16,67 +19,126 @@ import (
 var (
 	resize string
 	clip   string
+	format string
 )
 
 func init() {
 	flag.StringVar(&resize, "resize", "", "出力する画像サイズ（`幅[px|%]x高さ[px|%]`）")
 	flag.StringVar(&clip, "clip", "", "切り取る画像サイズ（`幅[px|%]x高さ[px|%]`）")
+	flag.StringVar(&format, "format", "", "ディレクトリ指定の場合に、変換する画像ファイルのフォーマット（`png|jpeg|jpg`）")
 	flag.Parse()
 }
 
-func run() int {
-	args := flag.Args()
-	if len(args) < 2 {
-		fmt.Fprintf(os.Stderr, "画像ファイルを指定してください。")
-		return 1
-	}
+func convert(dst, src string) error {
 
-	src, err := os.Open(args[0])
+	sf, err := os.Open(src)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "画像ファイルが開けませんでした。%s", args[0])
-		return 1
+		return fmt.Errorf("画像ファイルが開けませんでした。%s", src)
 	}
-	defer src.Close()
+	defer sf.Close()
 
-	dst, err := os.Create(args[1])
+	df, err := os.Create(dst)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "画像ファイルを書き出せませんでした。%s", args[1])
-		return 1
+		return fmt.Errorf("画像ファイルを書き出せませんでした。%s", dst)
 	}
-	defer dst.Close()
+	defer df.Close()
 
-	_img, _, err := image.Decode(src)
+	_img, _, err := image.Decode(sf)
+	if err != nil {
+		return err
+	}
 	img := &imgconv.Image{_img}
 
 	if resize != "" {
 		if err := img.Resize(resize); err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-			return 1
+			return fmt.Errorf("%s\n", err.Error())
 		}
 	}
 
 	if clip != "" {
 		if err := img.Clip(clip); err != nil {
-			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-			return 1
+			return fmt.Errorf("%s\n", err.Error())
 		}
 	}
 
-	switch strings.ToLower(path.Ext(args[1])) {
+	switch strings.ToLower(path.Ext(dst)) {
 	case ".png":
-		err = png.Encode(dst, img)
+		err = png.Encode(df, img)
 	case ".jpeg", ".jpg":
-		err = jpeg.Encode(dst, img, &jpeg.Options{jpeg.DefaultQuality})
+		err = jpeg.Encode(df, img, &jpeg.Options{jpeg.DefaultQuality})
 	}
 
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "画像ファイルを書き出せませんでした。%s", args[1])
-		return 1
+		return fmt.Errorf("画像ファイルを書き出せませんでした。%s", dst)
 	}
 
-	return 0
+	return nil
+}
+
+type file string
+
+func (f file) Ext() string {
+	return path.Ext(string(f))
+}
+
+func (f file) Dir() string {
+	return path.Dir(string(f))
+}
+
+func (f file) Name() string {
+	return strings.Replace(path.Base(string(f)), f.Ext(), "", -1)
+}
+
+func run() error {
+	args := flag.Args()
+	if len(args) < 2 {
+		return fmt.Errorf("画像ファイルを指定してください。")
+	}
+
+	info, err := os.Stat(args[0])
+	if os.IsNotExist(err) {
+		return fmt.Errorf("画像ファイルが存在しません。%s", args[0])
+	}
+
+	// ディレクトリの場合
+	if info.IsDir() {
+		t, err := template.New("dst").Parse(args[1])
+		if err != nil {
+			return err
+		}
+
+		return filepath.Walk(args[0], func(p string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if info.IsDir() {
+				return nil
+			}
+
+			ext := strings.ToLower(path.Ext(p))
+			if format != "" {
+				if ext != "."+format {
+					return nil
+				}
+			} else if ext != ".png" && ext != "jpg" && ext != "jpeg" {
+				return nil
+			}
+
+			var buf bytes.Buffer
+			t.Execute(&buf, file(p))
+
+			return convert(buf.String(), p)
+		})
+	}
+
+	// ファイルの場合
+	return convert(args[1], args[0])
 }
 
 func main() {
-	os.Exit(run())
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+		os.Exit(1)
+	}
 }
