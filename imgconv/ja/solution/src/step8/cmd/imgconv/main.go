3c523b0d468b4c060f2981538f31bbed3e3396bd
Takuya Ueda <uedatakuya@gmail.com>
2015-10-27 01:15:21 +0900

    step8修正

--- imgconv/ja/solution/src/step8/cmd/imgconv/main.go
+++ imgconv/ja/solution/src/step8/cmd/imgconv/main.go
@@ -78,10 +78,12 @@ func convert(dst, src string) error {
 
 type file string
 
+// TODO: 拡張子を返すメソッドを作る。
 func (f file) Ext() string {
 	return path.Ext(string(f))
 }
 
+// TODO: ディレクトリを返すメソッドを作る。
 func (f file) Dir() string {
 	return path.Dir(string(f))
 }
@@ -96,8 +98,9 @@ func run() error {
 		return fmt.Errorf("画像ファイルを指定してください。")
 	}
 
+	// TODO: 入力ファイルのファイル情報を取得する。
 	info, err := os.Stat(args[0])
-	if os.IsNotExist(err) {
+	if /* TODO: 画像ファイルが存在しない場合  */ os.IsNotExist(err) {
 		return fmt.Errorf("画像ファイルが存在しません。%s", args[0])
 	}
 
@@ -108,16 +111,19 @@ func run() error {
 			return err
 		}
 
-		return filepath.Walk(args[0], func(p string, info os.FileInfo, err error) error {
+		return filepath.Walk(args[0], func( /* TODO: 引数を埋める */ p string, info os.FileInfo, err error) error {
 			if err != nil {
 				return err
 			}
 
+			// TODO: ディレクトリだったら何もしない。
 			if info.IsDir() {
 				return nil
 			}
 
 			ext := strings.ToLower(path.Ext(p))
+			// TODO: 拡張子がformatで指定されたものでなければ何もしない。
+			// formatで何もしてなければ、拡張子が".png"、".jpg"、".jpeg"以外は何もしない。
 			if format != "" {
 				if ext != "."+format {
 					return nil
@@ -127,8 +133,12 @@ func run() error {
 			}
 
 			var buf bytes.Buffer
+			// TODO: pをfile型に変換して、テンプレートに埋め込む。
+			// file型に変換すると、DirやExt、Nameなどのメソッドが使用できる。
+			// テンプレートの展開先は、bufにする。
 			t.Execute(&buf, file(p))
 
+			// TODO: ファイル1つを変換する。
 			return convert(buf.String(), p)
 		})
 	}
