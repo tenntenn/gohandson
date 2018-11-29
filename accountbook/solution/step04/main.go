beb122a6b37363b4e18ce160616b06ffd0870d21
Takuya Ueda <uedatakuya@gmail.com>
2018-11-30 00:24:34 +0900

    Add document for step04

--- accountbook/solution/step04/main.go
+++ accountbook/solution/step04/main.go
@@ -1,3 +1,5 @@
+// STEP04: 複数データの記録
+
 package main
 
 import "fmt"
@@ -8,13 +10,22 @@ type Item struct {
 }
 
 func main() {
+
+	// 複数のItem型の値を記録するために
+	// itemsという名前のItem型のスライスの変数を定義
 	var items []Item
 
+	// 入力するデータの件数を入力する
 	var n int
 	fmt.Print("何件入力しますか>")
 	fmt.Scan(&n)
 
-	for i := 0; i < n; i++ {
+	// 長さ0で容量がnのスライスを作る
+	items := make([]Item, 0, n)
+
+	// iが0からitemsの容量-1の間繰り返す(n回繰り返す）
+	// cap(items)はitemsの容量を返す
+	for i := 0; i < cap(items); i++ {
 		items = inputItem(items)
 	}
 
@@ -22,7 +33,9 @@ func main() {
 	showItems(items)
 }
 
-// 入力
+// 入力を行う関数
+// 追加を行うItemのスライスを受け取る
+// 新しく入力したItemをスライスに追加して返す
 func inputItem(items []Item) []Item {
 	var item Item
 
@@ -32,16 +45,22 @@ func inputItem(items []Item) []Item {
 	fmt.Print("値段>")
 	fmt.Scan(&item.Price)
 
+	// スライスに新しく入力したitemを追加する
 	items = append(items, item)
 
 	return items
 }
 
-// 一覧の表示
+// 一覧の表示を行う関数
 func showItems(items []Item) {
 	fmt.Println("===========")
+
+	// itemsの長さだけforを回す
+	// len(items)はitemsの長さを返す
 	for i := 0; i < len(items); i++ {
+		// items[i]はitemsのi番目の要素(0からスタートする)
 		fmt.Printf("%s:%d円\n", items[i].Category, items[i].Price)
 	}
+
 	fmt.Println("===========")
 }
