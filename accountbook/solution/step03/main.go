5455ea1dfab854b44cfb5ca0d524c7cde6d4ed1b
Takuya Ueda <uedatakuya@gmail.com>
2018-11-29 22:45:06 +0900

    Add documents

--- accountbook/solution/step03/main.go
+++ accountbook/solution/step03/main.go
@@ -1,21 +1,32 @@
+// STEP03: データの記録
+
 package main
 
 import "fmt"
 
+// 品目と値段を一緒に扱うために
+// Itemという構造体の型を定義する
 type Item struct {
+	// Categoryは文字列型のフィールド
 	Category string
-	Price    int
+	// Priceは整数型のフィールド
+	Price int
 }
 
 func main() {
 
+	// Item型のitemという名前の変数を定義する
 	var item Item
 
 	fmt.Print("品目>")
+	// 入力した値をitemのCategoryフィールドに入れる
 	fmt.Scan(&item.Category)
 
 	fmt.Print("値段>")
+	// 入力した値をitemのPriceフィールドに入れる
 	fmt.Scan(&item.Price)
 
+	// 品目に「コーヒー」、値段に「100」と入力した場合に
+	// 「コーヒーに100円使いました」と表示する
 	fmt.Printf("%sに%d円使いました\n", item.Category, item.Price)
 }
