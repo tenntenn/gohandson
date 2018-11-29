54b3950b450e0aa3698254aac470c4255737c45e
Takuya Ueda <uedatakuya@gmail.com>
2018-11-29 22:09:01 +0900

    Divide into functions

--- accountbook/solution/step04/main.go
+++ accountbook/solution/step04/main.go
@@ -15,19 +15,32 @@ func main() {
 	fmt.Scan(&n)
 
 	for i := 0; i < n; i++ {
-		var item Item
-
-		fmt.Print("品目>")
-		fmt.Scan(&item.Category)
-
-		fmt.Print("値段>")
-		fmt.Scan(&item.Price)
-
-		items = append(items, item)
+		items = inputItem(items)
 	}
 
+	// 表示
+	showItems(items)
+}
+
+// 入力
+func inputItem(items []Item) []Item {
+	var item Item
+
+	fmt.Print("品目>")
+	fmt.Scan(&item.Category)
+
+	fmt.Print("値段>")
+	fmt.Scan(&item.Price)
+
+	items = append(items, item)
+
+	return items
+}
+
+// 一覧の表示
+func showItems(items []Item) {
 	fmt.Println("===========")
-	for i := 0; i < n; i++ {
+	for i := 0; i < len(items); i++ {
 		fmt.Printf("%s:%d円\n", items[i].Category, items[i].Price)
 	}
 	fmt.Println("===========")
