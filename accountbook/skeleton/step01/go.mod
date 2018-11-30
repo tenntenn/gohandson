58d618e4020a710caa1433018ff999f911e52457
Takuya Ueda <uedatakuya@gmail.com>
2018-12-01 08:00:07 +0900

    Add step01 to skelton

---
+++ accountbook/skeleton/step01/go.mod
@@ -0,0 +1,1 @@
+module github.com/tenntenn/gohandson/accountbook/skeleton/step01
---
+++ accountbook/skeleton/step01/main.go
@@ -0,0 +1,10 @@
+// STEP01: Goに触れる
+
+// mainパッケージの定義
+package main
+
+// main関数から実行される
+func main() {
+	// 「Hello, 世界」と出力
+	println("Hello, 世界")
+}
