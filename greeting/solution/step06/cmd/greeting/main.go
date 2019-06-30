abe85ccda8e6667570c88ada4d6a85aba08b6fe2
Takuya Ueda <uedatakuya@gmail.com>
2019-06-30 12:09:47 +0900

    Add greeting/step06

---
+++ greeting/solution/step06/cmd/greeting/main.go
@@ -0,0 +1,19 @@
+// STEP06: テストのパッケージを分けよう
+
+// mainパッケージの定義
+package main
+
+import (
+	"os"
+
+	// greetingパッケージのインポート
+	"github.com/tenntenn/gohandson/greeting"
+)
+
+// main関数から実行される
+func main() {
+	// Greeting型の変数を定義
+	var g greeting.Greeting
+	// 引数にos.Stdoutを渡してDoメソッドを呼び出す
+	g.Do(os.Stdout)
+}
