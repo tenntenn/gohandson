033b18e93c3492a8581aa706d3c6483ae76bdcf7
Takuya Ueda <uedatakuya@gmail.com>
2019-11-17 03:59:26 +0900

    Fix step11

---
+++ accountbook/skeleton/step11/go.mod
@@ -0,0 +1,5 @@
+module github.com/tenntenn/gohandson/accountbook/solution/step10
+
+go 1.13
+
+require github.com/tenntenn/sqlite v1.0.0
---
+++ accountbook/skeleton/step11/main.go
@@ -0,0 +1,43 @@
+// STEP11: 集計ページの作成
+
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/tenntenn/sqlite"
+)
+
+func main() {
+
+	// データベースへ接続
+	// ドライバにはSQLiteを使って、
+	// accountbook.dbというファイルでデータベース接続を行う
+	db, err := sql.Open(sqlite.DriverName, "accountbook.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// AccountBookをNewAccountBookを使って作成
+	ab := NewAccountBook(db)
+
+	// テーブルを作成
+	if err := ab.CreateTable(); err != nil {
+		log.Fatal(err)
+	}
+
+	// HandlersをNewHandlersを使って作成
+	hs := NewHandlers(ab)
+
+	// ハンドラの登録
+	http.HandleFunc("/", hs.ListHandler)
+	http.HandleFunc("/save", hs.SaveHandler)
+	http.HandleFunc("/summary", hs.SummaryHandler)
+
+	fmt.Println("http://localhost:8080 で起動中...")
+	// HTTPサーバを起動する
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
