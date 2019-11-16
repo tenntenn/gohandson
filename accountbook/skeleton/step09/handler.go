c9522e77dab30c0d9ab18f1e5cbada86a2e9a430
Takuya Ueda <uedatakuya@gmail.com>
2019-11-17 03:54:32 +0900

    Fix step09

--- accountbook/skeleton/step09/handler.go
+++ accountbook/skeleton/step09/handler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"html/template"
 	"net/http"
-	"strconv"
 )
 
 // HTTPハンドラを集めた型
@@ -25,21 +24,12 @@ var listTmpl = template.Must(template.New("list").Parse(`<!DOCTYPE html>
 	</head>
 	<body>
 		<h1>家計簿</h1>
-		<h2>入力</h2>
-		<form method="post" action="/save">
-			<label for="category">品目</label>
-			<input name="category" type="text">
-			<label for="price">値段</label>
-			<input name="price" type="number">
-			<input type="submit" value="保存">
-		</form>
-
 		<h2>最新{{len .}}件</h2>
 		{{- if . -}}
 		<table border="1">
 			<tr><th>品目</th><th>値段</th></tr>
 			{{- range .}}
-			<tr><td>{{.Category}}</td><td>{{.Price}}円</td></tr>
+			<tr><td>{{- /* TODO: 品目を埋め込む */ -}}</td><td>{{.Price}}円</td></tr>
 			{{- end}}
 		</table>
 		{{- else}}
@@ -51,50 +41,15 @@ var listTmpl = template.Must(template.New("list").Parse(`<!DOCTYPE html>
 
 // 最新の入力データを表示するハンドラ
 func (hs *Handlers) ListHandler(w http.ResponseWriter, r *http.Request) {
-	// 最新の10件を取得する
-	items, err := hs.ab.GetItems(10)
+	// TODO: 最新の10件を取得し、itemsに入れる
 	if err != nil {
+		// TODO: http.Errorを使って、InternalServerErrorでエラーを返す
+		return
+	}
+
+	// TODO: 取得したitemsをテンプレートに埋め込む
+	if /* ここに書く */; err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// 取得したitemsをテンプレートに埋め込む
-	if err := listTmpl.Execute(w, items); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-}
-
-// 保存
-func (hs *Handlers) SaveHandler(w http.ResponseWriter, r *http.Request) {
-	if /* TODO: r.MethodがPOST（http.MethodPost）か調べる */ {
-		code := http.StatusMethodNotAllowed
-		http.Error(w, http.StatusText(code), code)
-		return
-	}
-
-	// TODO: フォームから送られてきた品目を取得して、categoryに入れる
-	if category == "" {
-		http.Error(w, "品目が指定されていません", http.StatusBadRequest)
-		return
-	}
-
-	price, err := strconv.Atoi(r.FormValue("price"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	item := &Item{
-		Category: category,
-		Price:    price,
-	}
-
-	// TODO: itemをデータベースに保存する
-	if /* ここに追加 */ err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	http.Redirect(w, r, "/", http.StatusFound)
 }
